1ad8d56195b265a8570b37a1d3faa140d4c78cdd
Shad Storhaug <shad@shadstorhaug.com>
2021-04-19 09:12:19 +0700

    RandomStrings: Changed to expression-style syntax

--- src/RandomizedTesting.Generators/RandomStrings.cs
+++ src/RandomizedTesting.Generators/RandomStrings.cs
@@ -16,20 +16,20 @@ namespace RandomizedTesting.Generators
         // randomAsciiOfLengthBetween
         // randomAsciiOfLength
 
-        public static string RandomAsciiLettersOfLengthBetween(Random r, int minCodeUnits, int maxCodeUnits) { return asciiLettersGenerator.OfCodeUnitsLength(r, minCodeUnits, maxCodeUnits); }
-        public static string RandomAsciiLettersOfLength(Random r, int codeUnits) { return asciiLettersGenerator.OfCodeUnitsLength(r, codeUnits, codeUnits); }
+        public static string RandomAsciiLettersOfLengthBetween(Random r, int minCodeUnits, int maxCodeUnits) => asciiLettersGenerator.OfCodeUnitsLength(r, minCodeUnits, maxCodeUnits);
+        public static string RandomAsciiLettersOfLength(Random r, int codeUnits) => asciiLettersGenerator.OfCodeUnitsLength(r, codeUnits, codeUnits);
 
-        public static string RandomAsciiAlphanumOfLengthBetween(Random r, int minCodeUnits, int maxCodeUnits) { return asciiAlphanumGenerator.OfCodeUnitsLength(r, minCodeUnits, maxCodeUnits); }
-        public static string RandomAsciiAlphanumOfLength(Random r, int codeUnits) { return asciiAlphanumGenerator.OfCodeUnitsLength(r, codeUnits, codeUnits); }
+        public static string RandomAsciiAlphanumOfLengthBetween(Random r, int minCodeUnits, int maxCodeUnits) => asciiAlphanumGenerator.OfCodeUnitsLength(r, minCodeUnits, maxCodeUnits);
+        public static string RandomAsciiAlphanumOfLength(Random r, int codeUnits) => asciiAlphanumGenerator.OfCodeUnitsLength(r, codeUnits, codeUnits);
 
-        public static string RandomUnicodeOfLengthBetween(Random r, int minCodeUnits, int maxCodeUnits) { return unicodeGenerator.OfCodeUnitsLength(r, minCodeUnits, maxCodeUnits); }
-        public static string RandomUnicodeOfLength(Random r, int codeUnits) { return unicodeGenerator.OfCodeUnitsLength(r, codeUnits, codeUnits); }
-        public static string RandomUnicodeOfCodepointLengthBetween(Random r, int minCodePoints, int maxCodePoints) { return unicodeGenerator.OfCodePointsLength(r, minCodePoints, maxCodePoints); }
-        public static string RandomUnicodeOfCodepointLength(Random r, int codePoints) { return unicodeGenerator.OfCodePointsLength(r, codePoints, codePoints); }
+        public static string RandomUnicodeOfLengthBetween(Random r, int minCodeUnits, int maxCodeUnits) => unicodeGenerator.OfCodeUnitsLength(r, minCodeUnits, maxCodeUnits);
+        public static string RandomUnicodeOfLength(Random r, int codeUnits) => unicodeGenerator.OfCodeUnitsLength(r, codeUnits, codeUnits);
+        public static string RandomUnicodeOfCodepointLengthBetween(Random r, int minCodePoints, int maxCodePoints) => unicodeGenerator.OfCodePointsLength(r, minCodePoints, maxCodePoints);
+        public static string RandomUnicodeOfCodepointLength(Random r, int codePoints) => unicodeGenerator.OfCodePointsLength(r, codePoints, codePoints);
 
-        public static string RandomRealisticUnicodeOfLengthBetween(Random r, int minCodeUnits, int maxCodeUnits) { return realisticUnicodeGenerator.OfCodeUnitsLength(r, minCodeUnits, maxCodeUnits); }
-        public static string RandomRealisticUnicodeOfLength(Random r, int codeUnits) { return realisticUnicodeGenerator.OfCodeUnitsLength(r, codeUnits, codeUnits); }
-        public static string RandomRealisticUnicodeOfCodepointLengthBetween(Random r, int minCodePoints, int maxCodePoints) { return realisticUnicodeGenerator.OfCodePointsLength(r, minCodePoints, maxCodePoints); }
-        public static string RandomRealisticUnicodeOfCodepointLength(Random r, int codePoints) { return realisticUnicodeGenerator.OfCodePointsLength(r, codePoints, codePoints); }
+        public static string RandomRealisticUnicodeOfLengthBetween(Random r, int minCodeUnits, int maxCodeUnits) => realisticUnicodeGenerator.OfCodeUnitsLength(r, minCodeUnits, maxCodeUnits);
+        public static string RandomRealisticUnicodeOfLength(Random r, int codeUnits) => realisticUnicodeGenerator.OfCodeUnitsLength(r, codeUnits, codeUnits);
+        public static string RandomRealisticUnicodeOfCodepointLengthBetween(Random r, int minCodePoints, int maxCodePoints) => realisticUnicodeGenerator.OfCodePointsLength(r, minCodePoints, maxCodePoints);
+        public static string RandomRealisticUnicodeOfCodepointLength(Random r, int codePoints) => realisticUnicodeGenerator.OfCodePointsLength(r, codePoints, codePoints);
     }
 }
